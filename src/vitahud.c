be8b45928f2bf0bbf927939385d4db47f64dfc60
Eywa <hwtc5gtd6q@privaterelay.appleid.com>
2026-05-01 07:23:39 -0500

    Update vitahud.c

--- src/vitahud.c
+++ src/vitahud.c
@@ -76,7 +76,99 @@ static void handle_toggle(void) {
     last_combo_down = combo_down;
 }
 
-static void draw_test_box(void) {
+static unsigned char font5x7(char c, int row) {
+    switch (c) {
+        case 'V': {
+            static const unsigned char g[7] = {
+                0x11, 0x11, 0x11, 0x11, 0x0A, 0x0A, 0x04
+            };
+            return g[row];
+        }
+
+        case 'i': {
+            static const unsigned char g[7] = {
+                0x04, 0x00, 0x0C, 0x04, 0x04, 0x04, 0x0E
+            };
+            return g[row];
+        }
+
+        case 't': {
+            static const unsigned char g[7] = {
+                0x04, 0x04, 0x1F, 0x04, 0x04, 0x14, 0x08
+            };
+            return g[row];
+        }
+
+        case 'a': {
+            static const unsigned char g[7] = {
+                0x00, 0x00, 0x0E, 0x01, 0x0F, 0x11, 0x0F
+            };
+            return g[row];
+        }
+
+        case 'H': {
+            static const unsigned char g[7] = {
+                0x11, 0x11, 0x11, 0x1F, 0x11, 0x11, 0x11
+            };
+            return g[row];
+        }
+
+        case 'U': {
+            static const unsigned char g[7] = {
+                0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x0E
+            };
+            return g[row];
+        }
+
+        case 'D': {
+            static const unsigned char g[7] = {
+                0x1E, 0x11, 0x11, 0x11, 0x11, 0x11, 0x1E
+            };
+            return g[row];
+        }
+
+        default:
+            return 0x00;
+    }
+}
+
+static void draw_char(unsigned int *pixels, int pitch, int x, int y, char c, unsigned int color, int scale) {
+    for (int row = 0; row < 7; row++) {
+        unsigned char bits = font5x7(c, row);
+
+        for (int col = 0; col < 5; col++) {
+            if (bits & (1 << (4 - col))) {
+                for (int sy = 0; sy < scale; sy++) {
+                    for (int sx = 0; sx < scale; sx++) {
+                        pixels[(y + row * scale + sy) * pitch + (x + col * scale + sx)] = color;
+                    }
+                }
+            }
+        }
+    }
+}
+
+static int text_width(const char *text, int scale) {
+    int count = 0;
+
+    while (text[count]) {
+        count++;
+    }
+
+    return count * 6 * scale;
+}
+
+static void draw_text(unsigned int *pixels, int pitch, int x, int y, const char *text, unsigned int color, int scale) {
+    int cursor_x = x;
+
+    while (*text) {
+        draw_char(pixels, pitch, cursor_x, y, *text, color, scale);
+        cursor_x += 6 * scale;
+        text++;
+    }
+}
+
+static void draw_hud_text(void) {
     SceDisplayFrameBuf fb;
     fb.size = sizeof(SceDisplayFrameBuf);
 
@@ -96,23 +188,24 @@ static void draw_test_box(void) {
     int screen_h = fb.height;
     int pitch = fb.pitch;
 
-    int box_w = 100;
-    int box_h = 18;
+    const char *text = "VitaHUD";
+    int scale = 2;
 
-    int start_x = screen_w - box_w - 8;
-    int start_y = screen_h - box_h - 8;
+    int w = text_width(text, scale);
+    int h = 7 * scale;
+
+    int start_x = screen_w - w - 8;
+    int start_y = screen_h - h - 8;
 
     if (start_x < 0 || start_y < 0) {
         return;
     }
 
-    unsigned int color = 0xFF00FF00;
+    unsigned int white = 0xFFFFFFFF;
+    unsigned int shadow = 0xFF000000;
 
-    for (int y = 0; y < box_h; y++) {
-        for (int x = 0; x < box_w; x++) {
-            pixels[(start_y + y) * pitch + (start_x + x)] = color;
-        }
-    }
+    draw_text(pixels, pitch, start_x + 1, start_y + 1, text, shadow, scale);
+    draw_text(pixels, pitch, start_x, start_y, text, white, scale);
 }
 
 static int hud_thread(SceSize args, void *argp) {
@@ -129,7 +222,7 @@ static int hud_thread(SceSize args, void *argp) {
             char time_text[9];
             get_time_12h(time_text);
 
-            draw_test_box();
+            draw_hud_text();
 
             (void)battery;
             (void)time_text;
