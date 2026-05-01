3d898a5d8a95932e1eac611fe1dc9c6c05d5cae2
Eywa186 <hwtc5gtd6q@privaterelay.appleid.com>
2026-04-30 22:42:36 -0500

    Update vitahud.c

--- src/vitahud.c
+++ src/vitahud.c
@@ -3,7 +3,6 @@
 #include <psp2/ctrl.h>
 #include <psp2/power.h>
 #include <psp2/rtc.h>
-#include <stdio.h>
 
 #define TOGGLE_COMBO (SCE_CTRL_LTRIGGER | SCE_CTRL_RTRIGGER | SCE_CTRL_SELECT)
 
@@ -13,6 +12,37 @@ static int last_combo_down = 0;
 static unsigned int frame_count = 0;
 static unsigned int fps_value = 0;
 static SceUInt64 last_tick = 0;
+
+static void put_2digits(char *out, int value) {
+    out[0] = '0' + ((value / 10) % 10);
+    out[1] = '0' + (value % 10);
+}
+
+static void get_time_12h(char *out) {
+    SceDateTime time;
+    sceRtcGetCurrentClockLocalTime(&time);
+
+    int hour = time.hour;
+    int is_pm = 0;
+
+    if (hour >= 12) {
+        is_pm = 1;
+    }
+
+    hour = hour % 12;
+
+    if (hour == 0) {
+        hour = 12;
+    }
+
+    put_2digits(&out[0], hour);
+    out[2] = ':';
+    put_2digits(&out[3], time.minute);
+    out[5] = ' ';
+    out[6] = is_pm ? 'P' : 'A';
+    out[7] = 'M';
+    out[8] = '\0';
+}
 
 static void update_fps(void) {
     SceRtcTick tick;
@@ -45,26 +75,6 @@ static void handle_toggle(void) {
     last_combo_down = combo_down;
 }
 
-static void get_time_12h(char *out, int out_size) {
-    SceDateTime time;
-    sceRtcGetCurrentClockLocalTime(&time);
-
-    int hour = time.hour;
-    const char *ampm = "AM";
-
-    if (hour >= 12) {
-        ampm = "PM";
-    }
-
-    hour = hour % 12;
-
-    if (hour == 0) {
-        hour = 12;
-    }
-
-    snprintf(out, out_size, "%02d:%02d %s", hour, time.minute, ampm);
-}
-
 static int hud_thread(SceSize args, void *argp) {
     (void)args;
     (void)argp;
@@ -76,16 +86,17 @@ static int hud_thread(SceSize args, void *argp) {
         if (hud_enabled) {
             int battery = scePowerGetBatteryLifePercent();
 
-            char time_text[16];
-            get_time_12h(time_text, sizeof(time_text));
+            char time_text[9];
+            get_time_12h(time_text);
 
             /*
-             * v1 logic is active here:
+             * v1 logic:
              * fps_value = live FPS
              * battery = live battery %
              * time_text = 12-hour time
              *
-             * Drawing hook will be added after this builds cleanly.
+             * Next step after clean build:
+             * add real overlay drawing.
              */
             (void)battery;
             (void)time_text;
@@ -109,11 +120,11 @@ int module_start(SceSize args, void *argp) {
         0x10000,
         0,
         0,
-        NULL
+        0
     );
 
     if (thid >= 0) {
-        sceKernelStartThread(thid, 0, NULL);
+        sceKernelStartThread(thid, 0, 0);
     }
 
     return SCE_KERNEL_START_SUCCESS;
@@ -122,5 +133,6 @@ int module_start(SceSize args, void *argp) {
 int module_stop(SceSize args, void *argp) {
     (void)args;
     (void)argp;
+
     return SCE_KERNEL_STOP_SUCCESS;
 }
